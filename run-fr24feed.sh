76b1e9bf9c2a3178c20eca92747f124699ac0405
James Cohen <james.cohen@gmail.com>
2018-09-20 23:21:13 +0100

    fixed dodgy logic with config symlink/management

--- run-fr24feed.sh
+++ run-fr24feed.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 
-# if a volume has been mounted copy in the feed file and symlink it
+# if a volume has been mounted copy in the config file and symlink it
 # so the config persists container restarts
-if [ -d /etc/fr24feed && -f /etc/fr24feed.ini ]; then
-    mv /etc/fr24feed.ini /etc/fr24feed/fr24feed.ini
+if [ -d /etc/fr24feed ]; then
+    echo Found /etc/fr24feed directory
+    if [ -f /etc/fr24feed/fr24feed.ini ]; then
+        echo Removing default config
+        rm /etc/fr24feed.ini
+    else
+        echo Copying default config to directory
+        mv /etc/fr24feed.ini /etc/fr24feed/
+    fi
     ln -s /etc/fr24feed/fr24feed.ini /etc/fr24feed.ini
 fi
 
